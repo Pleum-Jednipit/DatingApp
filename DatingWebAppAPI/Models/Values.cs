479a9d73d66eac29f68899d54784b36ac9849f83
P!eum <jednitan10@gmail.com>
2020-04-21 21:28:44 +0700

    Initial Commit

---
+++ DatingWebAppAPI/Models/Values.cs
@@ -0,0 +1,17 @@
+namespace DatingApp.API.Models
+{
+    public class Values
+    {
+        public int Id
+        {
+            get;
+            set;
+        }
+
+        public string name
+        {
+            get;
+            set;
+        }
+    }
+}
