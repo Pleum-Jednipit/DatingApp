493eca87a1a880ad9df55f1818bf3686b48d4041
P!eum <jednitan10@gmail.com>
2020-05-03 20:46:41 +0700

    Add API for user data, update database and add mapper

--- DatingWebAppAPI/DTO/UserForDetailedDto.cs
+++ DatingWebAppAPI/DTO/UserForDetailedDto.cs
@@ -1,20 +1,17 @@
 using System;
 using System.Collections.Generic;
+using DatingWebAppAPI.Models;
 
-namespace DatingWebAppAPI.Models
+namespace DatingWebAppAPI.DTO
 {
-    public class User
+    public class UserForDetailedDto
     {
         public int Id { get; set; }
         public string Username { get; set; }
 
-        public byte[] PasswordHash { get; set; }
-
-        public byte[] PasswordSalt { get; set; }   
-
         public string Gender { get; set; }
 
-        public DateTime DateOfBirth { get; set; }
+        public int Age { get; set; }
 
         public string KnownAs { get; set; }
 
@@ -32,6 +29,8 @@ namespace DatingWebAppAPI.Models
 
         public string Country { get; set; }
 
-        public ICollection<Photo> Photos { get; set; }    
+        public string PhotoUrl { get; set; }
+
+        public ICollection<PhotosForDetailedDto> Photos { get; set; }
     }
 }
--- DatingWebAppAPI/DTO/UserForListDto.cs
+++ DatingWebAppAPI/DTO/UserForListDto.cs
@@ -1,20 +1,15 @@
 using System;
-using System.Collections.Generic;
 
-namespace DatingWebAppAPI.Models
+namespace DatingWebAppAPI.DTO
 {
-    public class User
+    public class UserForListDto
     {
         public int Id { get; set; }
         public string Username { get; set; }
 
-        public byte[] PasswordHash { get; set; }
-
-        public byte[] PasswordSalt { get; set; }   
-
         public string Gender { get; set; }
 
-        public DateTime DateOfBirth { get; set; }
+        public int Age { get; set; }
 
         public string KnownAs { get; set; }
 
@@ -32,6 +27,6 @@ namespace DatingWebAppAPI.Models
 
         public string Country { get; set; }
 
-        public ICollection<Photo> Photos { get; set; }    
+        public string PhotoUrl { get; set; }
     }
 }
